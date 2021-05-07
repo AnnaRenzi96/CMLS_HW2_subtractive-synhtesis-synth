bd5ad351c23d8c57939bb45334d12cc0e97fa90d
rickykubler <82145414+rickykubler@users.noreply.github.com>
2021-05-07 12:03:06 +0200

    editor osc + processor osc
    processor osc not working

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,48 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+BasicOscillatorAudioProcessorEditor::BasicOscillatorAudioProcessorEditor (BasicOscillatorAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+
+    juce::StringArray choices{ "Sine", "Saw", "Square" };
+    oscWaveSelector.addItemList(choices, 1); // why 1?
+    addAndMakeVisible(oscWaveSelector);
+    oscWaveSelectorAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment>(audioProcessor.apvts,"OSC1WAVETYPE", oscWaveSelector);
+
+}
+
+BasicOscillatorAudioProcessorEditor::~BasicOscillatorAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void BasicOscillatorAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void BasicOscillatorAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+
+    oscWaveSelector.setBounds(10, 10, 100, 30);
+}
